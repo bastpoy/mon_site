fd849732dd08646581d7b986425d5b446e7f7f7e
bastpoy <55000103+bastpoy@users.noreply.github.com>
2021-01-31 17:50:10 +0000

    Add files via upload

--- java.js
+++ java.js
@@ -1,10 +1,12 @@
-const instagram=document.querySelector('.instagram');
-const facebook=document.querySelector('.facebook');
-const linkedin=document.querySelector('.linkedin');
-const twitter=document.querySelector('.twitter');
+const presentationTop = document.querySelector('.texte_presentation');
+const parcoursTop = document.querySelector('.texte_parcours');
+const interetTop = document.querySelector('.texte_interet');
+const projetTop = document.querySelector('.texte_projet');
+
 
 // les fonctions permettant de renvoyer sur mes réseaux
-const reseau= function(ev)
+// en mode blank dans un nouvel onglet
+const functionClick= function(ev)
 {
     if(ev.target.className==="utilisateur instagram")
     {
@@ -21,6 +23,26 @@ const reseau= function(ev)
     {
         window.open('https://twitter.com/zigotys', '_blank');
     }
+
+    //tout ce qui concerne la navigation a gauche
+    //Quand je clique ca va directement dans la div
+    if(ev.target.className==="gauche presentation")
+    {
+        window.scrollTo(0,presentationTop.offsetTop);
+        console.log('salut');
+    }
+    if(ev.target.className==="gauche parcours")
+    {
+        window.scrollTo(0,parcoursTop.offsetTop);
+    }
+    if(ev.target.className==="gauche interet")
+    {
+        window.scrollTo(0,interetTop.offsetTop);
+    }
+    if(ev.target.className==="gauche projet")
+    {
+        window.scrollTo(0,projetTop.offsetTop);
+    }
 }
 
-document.addEventListener('click',reseau);
+document.addEventListener('click',functionClick);
